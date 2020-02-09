3469f1b7ea1f4a30ccd06ca10324ad04cad1d448
mrmm2703 <mrmm2703@gmail.com>
2020-02-09 10:41:30 +0000

    Added School view.

--- app/src/main/java/com/morahman/mentalscreen/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/morahman/mentalscreen/ui/slideshow/SlideshowFragment.java
@@ -1,5 +1,7 @@
 package com.morahman.mentalscreen.ui.slideshow;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,25 +13,42 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 import com.morahman.mentalscreen.R;
+import com.morahman.mentalscreen.ui.main.SectionsPagerAdapterSchool;
+import com.morahman.mentalscreen.ui.main.SectionsPagerAdapterYear;
 
 public class SlideshowFragment extends Fragment {
 
+
+    SharedPreferences sharedPreferences;
+    String class_;
+    String id;
     private SlideshowViewModel slideshowViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_school, container, false);
+//        final TextView textView = root.findViewById(R.id.text_slideshow);
+//        slideshowViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        sharedPreferences = getContext().getSharedPreferences("settings", Context.MODE_PRIVATE);
+        class_ = sharedPreferences.getString("class", "null");
+        id = sharedPreferences.getString("class", null);
+        SectionsPagerAdapterSchool sectionsPagerAdapter = new SectionsPagerAdapterSchool(getContext(), getFragmentManager());
+        ViewPager viewPager = root.findViewById(R.id.view_pager_school);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        viewPager.setOffscreenPageLimit(3);
+        TabLayout tabs = root.findViewById(R.id.tabs_school);
+        tabs.setupWithViewPager(viewPager);
         return root;
     }
 }
